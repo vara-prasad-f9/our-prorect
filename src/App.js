65bdda8b54848c9c969d86ddb2ccff32217c1c0e
Vara Prasad <ux@f9tech.com>
2024-07-25 14:05:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
